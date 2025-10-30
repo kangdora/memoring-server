687510036e1bab7540203b1f42b1b5fe9ca3ce2a
KangDora <ioprt123@gmail.com>
2025-10-30 14:05:41 +0900

    git init

---
+++ src/main/java/com/memoring/memoring_server/MemoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.memoring.memoring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memoring/memoring_server/MemoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoring.memoring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
