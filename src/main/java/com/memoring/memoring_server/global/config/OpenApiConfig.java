641696c7f8c2a4373d8a31d4bc28c6b8c2088950
KangDora <ioprt123@gmail.com>
2025-11-18 14:40:55 +0900

    feat: User swagger 추가

---
+++ src/main/java/com/memoring/memoring_server/global/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.memoring.memoring_server.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Memoring API 문서",
+                version = "v1",
+                description = "Memoring 서비스 사용자 관련 API 문서입니다."
+        )
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
+
