66830187230d5400c03dae97053a19d6c2c971ea
KangDora <ioprt123@gmail.com>
2025-11-24 10:41:28 +0900

    feat: s3부분 환경변수로 분리

--- src/main/java/com/memoring/memoring_server/global/storage/StorageConfig.java
+++ src/main/java/com/memoring/memoring_server/global/storage/StorageConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,12 +13,6 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class StorageConfig {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;  // 삭제
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;  // 삭제
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -25,12 +20,7 @@ public class StorageConfig {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                // .credentialsProvider(DefaultCredentialsProvider.create())
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -38,12 +28,7 @@ public class StorageConfig {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                // .credentialsProvider(DefaultCredentialsProvider.create())
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
